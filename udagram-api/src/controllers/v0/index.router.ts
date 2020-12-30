14087248aa6966bdf19ca320292f55f28da9e35d
Mike <mhayes30125@yahoo.com>
2020-12-29 19:33:39 -0500

    Added healthcheck route

--- udagram-api/src/controllers/v0/index.router.ts
+++ udagram-api/src/controllers/v0/index.router.ts
@@ -1,9 +1,11 @@
 import {Router, Request, Response} from 'express';
 import {FeedRouter} from './feed/routes/feed.router';
+import {HealthcheckRouter} from './health/routes/healthcheck.router';
 
 const router: Router = Router();
 
 router.use('/feed', FeedRouter);
+router.use('/healthcheck', HealthcheckRouter);
 
 router.get('/', async (req: Request, res: Response) => {
   res.send(`V0`);
