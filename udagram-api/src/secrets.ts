ae8070ff345c651143c4fdaa517acb225aef8170
Mike <mhayes30125@yahoo.com>
2021-01-01 11:56:32 -0500

    Updating Secrets Manager err handling

--- udagram-api/src/secrets.ts
+++ udagram-api/src/secrets.ts
@@ -9,26 +9,7 @@ export function setSecrets()
 
     client.getSecretValue({SecretId: "monolith_to_microservices_secret"}, function(err, data) {
         if (err) {
-            if (err.code === 'DecryptionFailureException')
-                // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-                // Deal with the exception here, and/or rethrow at your discretion.
-                throw err;
-            else if (err.code === 'InternalServiceErrorException')
-                // An error occurred on the server side.
-                // Deal with the exception here, and/or rethrow at your discretion.
-                throw err;
-            else if (err.code === 'InvalidParameterException')
-                // You provided an invalid value for a parameter.
-                // Deal with the exception here, and/or rethrow at your discretion.
-                throw err;
-            else if (err.code === 'InvalidRequestException')
-                // You provided a parameter value that is not valid for the current state of the resource.
-                // Deal with the exception here, and/or rethrow at your discretion.
-                throw err;
-            else if (err.code === 'ResourceNotFoundException')
-                // We can't find the resource that you asked for.
-                // Deal with the exception here, and/or rethrow at your discretion.
-                throw err;
+            console.log(JSON.stringify(err));
         }
         else {
             // Decrypts secret using the associated KMS CMK.
